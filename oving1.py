1681f75e54a55cd560c266c6b08fa664c86fab11
Katrine Roland <katrine.roland@gmail.com>
2013-08-24 14:51:44 +0200

    first try

---
+++ oving1.py
@@ -0,0 +1,29 @@
+from sys import stdin
+
+class Kubbe:
+    vekt = None
+    neste = None
+    def __init__(self, vekt):
+        self.vekt = vekt
+        self.neste = None
+
+def spor(kubbe):
+    tyngst = 0
+    while (kubbe.neste != None)
+        if kubbe.vekt > tyngst:
+            tyngst = kubbe.vekt
+        kubbe = kubbe.neste
+    return tyngst
+
+# Oppretter lenket liste
+forste = None
+siste = None
+for linje in stdin:
+    forrige_siste = siste
+    siste = Kubbe(int(linje))
+    if forste == None:
+        forste = siste
+    else:
+        forrige_siste.neste = siste
+
+print spor(forste)
